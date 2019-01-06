90af112b5fef38a557462f5639f9dbb8dbc29b1f
Brad Schrag <brad.schrag@gmail.com>
2019-01-06 04:15:30 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,12 @@ class Pokemon
   
   attr_accessor :id, :name, :type, :db, :hp
   
-  def initialize(id:, name:, type:, db:, hp:=nil)
-    args.each do |k, v|
-      self.send("#{k}=", v)
-    end
+  def initialize(id:, name:, type:, db:, hp: nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+    @hp = hp
   end
   
   def self.save(name, breed, db)
